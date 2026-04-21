c62c4748935ed02d7f56b2a6c7f2fd3a1fb589cf
Benito_De_Gennaro <benitodeg@gmail.com>
2026-04-21 17:50:41 +0200

    Create 07_classificazione.R

---
+++ code/07_classificazione.R
@@ -0,0 +1,62 @@
+#codice per calssificare i dati 
+library(terra)
+library(viridis)
+library(imageRy)
+
+#lista delle immagini 
+im.list()
+#impoirto il file del satellite solar horbiter
+sun<-im.import("Solar_Orbiter_s_first_views_of_the_Sun_pillars.jpg")
+
+#classifico l'immagine in base ai suo livelli 
+sun_c<-im.classify(sun)
+
+sun_c<-im.classify(sun, seed=3)
+sun_c<-im.classify(sun, seed=42)
+#importo l'immagine del gran canion 
+can<-im.import("dolansprings_oli_2013088_canyon_lrg.jpg")
+
+#classificazione del dato del gran canion
+can_c<-im.classify(can, seed=42, num_clusters=4)
+#per sapere il numero di pixell
+ncell(can)
+
+setwd("~/Desktop/Magistrale")
+
+#esempio del matogrosso 
+im.list
+m_06<-im.import("matogrosso_ast_2006209_lrg.jpg")
+m_92<-im.import()
+im.multiframe(1,2)
+ plot(m_06)
+plot(m_92)
+
+#classificazione 1992
+ mc_92<-im.classify(m_92, seed=42, num_cluster=2)
+levels(mc_92) <- data.frame(
+  value = c(1, 2),
+  label = c("forest", "human")
+)
+#lo rifaccio per il 2006
+mc_06<-im.classify(m_06, seed=42, num_cluster=2)
+levels(mc_06) <- data.frame(
+  value = c(2, 1),
+  label = c("forest", 
+)
+
+#calcoliamo le frequenze 
+freq(mc_92)
+#frequenza %
+freq(mc_92)/ncell(m_92)
+#f1992
+prop1992<-f_1992$count/ncell(mc_92)
+
+f_2006<-freq(mc_06)
+#frequenza %
+freq(mc_92)/ncell(m_92)
+#f1992
+prop1992<-f_1992$count/ncell(mc_92)
+tabella<-data.frame(
+  class=c("foresta","umana")
+          perc1992=c(83, 17),
+          perc2006=c(54,45))
