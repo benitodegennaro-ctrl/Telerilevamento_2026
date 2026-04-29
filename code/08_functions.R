6ae66050bd05b5cf80f0ad69846d106e29269011
Benito_De_Gennaro <benitodeg@gmail.com>
2026-04-29 17:17:54 +0200

    Update 08_functions.R

--- code/08_functions.R
+++ code/08_functions.R
@@ -5,6 +5,53 @@ somma<-function(x,y){
 }
 #differenza 
 differenza<-function(x,y){
-  z=x/y
+  z=x-y
   return(z)
   }
+#par(mfrow
+mf<-function(nrow,ncol){
+  par(mfrow=c(nx,ny))
+}
+
+mf<-function(nrow=1,ncol=2){
+  par(mfrow=c(nx,ny))
+}
+
+#if els
+numeri<- function(x){
+  if(x>0) {
+    print("questo numero e positivo, nooo?")
+}
+  else {
+    print("questo nuemro e negativo")
+    }
+  }
+
+numeri<- function(x){
+  if(x>0) {
+    print("questo numero e positivo, nooo?")
+}
+  else if(x<0) {
+    print("questo nuemro e negativo")
+    }
+    else {
+      print("zero non e ne negativo ne positivo")
+      }
+  }
+
+
+# cilci for 
+loop<-function(){
+  for(i in 1:10){
+    print(i)
+    }
+     }
+
+
+
+loop2<-function(){
+  for(i in 1:10){
+    op<- i*2
+    print(op)
+    }
+     }
