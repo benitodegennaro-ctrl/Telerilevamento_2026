7f2c355486c4bcdd908403618924da576ed60b8f
Benito_De_Gennaro <benitodeg@gmail.com>
2026-05-31 18:14:09 +0200

    Update Codice R per l'esame.R

--- code/Codice R per l'esame.R
+++ code/Codice R per l'esame.R
@@ -1,5 +1,6 @@
 # Imposta la cartella di lavoro per gestire correttamente l'input e l'output dei file del progetto
 setwd("~/Desktop/Progetto_ucraina.R")
+
 #caricamento dei pachetti 
 library(terra)     # Per gestire le immagini satellitari
 library(imageRy)   # Per facilitarmi il lavoro con le bande
@@ -7,11 +8,3 @@ library(ggplot2)   # Per fare i grafici
 library(patchwork) # Per mettere i grafici uno accanto all'altro
 library(viridis)   # Per i colori delle mappe (così si vedono bene)
 library(ggridges)  # Per i grafici a cresta (belli e utili)
-
-# Carico i pacchetti che mi servono per lavorare
-library(terra)      # Per gestire le immagini satellitari
-library(imageRy)    # Per facilitarmi il lavoro con le bande
-library(ggplot2)    # Per fare i grafici
-library(patchwork)  # Per mettere i grafici uno accanto all'altro
-library(viridis)    # Per i colori delle mappe (così si vedono bene)
-library(ggridges)   # Per i grafici a cresta (belli e utili)
