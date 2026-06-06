8309d54e64c6900ccd683ab656bbc2651fca86fd
Benito_De_Gennaro <benitodeg@gmail.com>
2026-06-06 14:14:46 +0200

    Update Codice R per l'esame.R

--- code/Esame_telerilevamento/Codice R per l'esame.R
+++ code/Esame_telerilevamento/Codice R per l'esame.R
@@ -33,7 +33,7 @@ im.multiframe(1,3) #divisione dell interfaccia grafica in 1 riga e tre colonne
 im.plotRGB(Ucraina_2021, r=3, g=2, b=1, title="Ucraina 2021 pre-conflitto") #Composizione spettrale nel dominio del visibile
 im.plotRGB(Ucraina_2022, r=3, g=2, b=1, title="Ucraina 2022 periodo critico") #Composizione spettrale nel dominio del visibile
 im.plotRGB(Ucraina_2026, r=3, g=2, b=1, title="Ucraina 2026 periodo attuale") #Composizione spettrale nel dominio del visibile
-dev.off()
+
 
 # Analisi della scomposizione spettrale multitemporale
 im.multiframe(3, 4) #suddivisione interfaccia grafica 
